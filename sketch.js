6e9cf2d3340892377b082e7b53e8064710ee4a36
bibi <gabrielanicodemos326@gmail.com>
2024-09-30 22:17:34 -0300

    alterei o codigo

--- sketch.js
+++ sketch.js
@@ -1,44 +1,47 @@
 let campoIdade;
-let campoAventura;
-let campoComedia;
 let campoFantasia;
-let campoAção;
-let campoTeen;
+let campoTerror;
+let campoAventura;
+let campoRomance;
+let campoFicçãoCientifica;
+let campoDrama;
+
 
 function setup() {
   createCanvas(800, 400);
-  createElement("h2", "RR recomenda filmes");
+  createElement("h2", "BIBI recomenda filmes");
   createSpan("Sua idade:");
   campoIdade = createInput("0");
-  campoAventura = createCheckbox("Gosta de filmes de Aventura ?");
-  campoComedia = createCheckbox("Gosta de filmes de Comedia ?");
-  campoTeen = createCheckbox("Gosta de filmes teen ?");
-  campoAção = createCheckbox("Gosta de filmes de Ação ?");
-  campoFantasia = createCheckbox("Gosta de fimes de Fantasia ?");
-  campoAnimação = createCheckbox("Gosta de fimes de Animação ?");
+  campoFantasia = createCheckbox("Gosta de filmes de Fantasia ?");
+  campoTerror = createCheckbox("Gosta de filmes de Terror ?");
+  campoAventura = createCheckbox("Gosta de filmes Aventura ?");
+  campoRomance = createCheckbox("Gosta de filmes de Romance ?");
+  campoFicçãoCientifica = createCheckbox("Gosta de fimes de Ficção Ciêntifica ?");
+  campoDrama = createCheckbox("Gosta de fimes de Drama ?");
 }
 
 function draw() {
-  background("#CBEFFF");
+  background("rgb(210,180,140)");
   let idade = campoIdade.value();
   let gostaDeFantasia = campoFantasia.checked();
+  let gostaDeTerror = campoTerror.checked();
   let gostaDeAventura = campoAventura.checked();
-  let gostaDeComedia = campoComedia.checked();
-  let gostaDeTeen = campoTeen.checked();
-  let gostaDeAção = campoAção.checked();
-  let gostaDeAnimação = campoAnimação.checked();
+  let gostaDeRomance = campoRomance.checked();
+  let gostaDeFicçãoCientifica = campoFicçãoCientifica.checked();
+  let gostaDeDrama = campoDrama.checked();
   let recomendacao = geraRecomendacao(
     idade,
     gostaDeFantasia,
-    gostaDeComedia,
-    gostaDeAção,
-    gostaDeAnimação,
+    gostaDeTerror,
+    gostaDeRomance,
+    gostaDeFicçãoCientifica,
     gostaDeAventura,
-    gostaDeTeen
+    gostaDeDrama
   );
-  fill(color(95, 0, 95));
+  
+ fill(color(120, 124, 107));
   textAlign(CENTER, CENTER);
-  textSize(30);
+  textSize(38);
   text(recomendacao, width / 2, height / 2);
 }
 
@@ -46,39 +49,39 @@ function geraRecomendacao(
   idade,
   gostaDeFantasia,
   gostaDeAventura,
-  gostaDeComedia,
-  gostaDeAção,
-  gostaDeAnimação,
-  gostaDeTeen
+  gostaDeRomance,
+  gostaDeTerror,
+  gostaDeFicçãoCientifica,
+  gostaDeDrama
 ) 
 {
-  if (idade >= 10) {
-    if (idade >= 14) {
-      if(gostaDeComedia || gostaDeAção) {
-      return "As branquelas";
+  if (idade >= 18) {
+    if (idade >= 16) {
+      if(gostaDeFicçãoCientifica || gostaDeFantasia) {
+     return "Shingueki no kiojin";      
       } else {
-        return "Gente grande";
+     return "Neon Genesis Evangelion";
       }
     } else {
-      if (idade >= 12) {
-        if(gostaDeTeen || gostaDeComedia) {
-          return "As branquelas";          
+      if (idade >= 14) {
+        if(gostaDeRomance || gostaDeAventura) {
+   return "Que chegue até você:Kimi ni Todoke";             
         } else{
-         return "Cruella";
+  return "The boy and the heron";        
         }
       } else {
-        if (gostaDeAnimação || gostaDeAção) {
-          return "Sonic";
+        if (gostaDeFantasia || gostaDeAventura) {
+          return "Your name?";
         } else {
-          return "Space jam";
+          return "Yuyu hakusho";
         }
       }
     }
   } else {
-    if (gostaDeAnimação || gostaDeComedia) {
-      return "Epá, cadê o Noé?";
+    if (gostaDeDrama || gostaDeTerror) {
+          return "Serial Experiments Lain";
     } else {
-      return "Soul";
+         return "Death note";
     }
   }
 }
